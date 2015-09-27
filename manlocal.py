7f7335927ba80af73b45a0e25973f9cbbeefd841
Jonas <jonas.kemper.biz@gmail.com>
2015-09-27 12:51:18 +0200

    added old FAQ. seems to lack style information.

---
+++ manlocal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "volunteer_planner.settings.local")
+    os.environ.setdefault("DATABASE_PW", "martin")
+    os.environ.setdefault("DB_USER", "martin")
+    os.environ.setdefault("EMAIL_ADDRESS", "")
+    os.environ.setdefault("EMAIL_HOST_PASSWORD", "")
+    os.environ.setdefault("SECRET_KEY", "1")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
--- non_logged_in_area/urls.py
+++ non_logged_in_area/urls.py
@@ -4,5 +4,6 @@ from .views import HomeView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(template_name="home.html"), name="home"),
+    url(r'^faq$', HomeView.as_view(template_name="faq.html"), name="faq"),
     url(r'^shelters-need-help/$', HomeView.as_view(template_name="shelters_need_help.html"), name="home"),
 ]
